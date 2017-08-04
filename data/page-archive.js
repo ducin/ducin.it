8241d26faf92a426ddbf453affbbd53ff5f35d30
Tomasz Ducin <tomasz.ducin@gmail.com>
2017-08-05 00:20:32 +0200

    popup window on maps

--- data/page-archive.js
+++ data/page-archive.js
@@ -3,12 +3,21 @@
 const $map = document.getElementById('map-canvas');
 const latlng = (lat, lng) => new google.maps.LatLng(lat, lng);
 
+const markerTpl = item => `<div class="marker">
+<h3>${item.venue.name}</h3>
+<h4>${item.venue.city}, ${item.venue.countryCode}</h4>
+<div><ul>
+  <li><i>${item.title}</i> at ${item.event}</li>
+</ul></div>
+</div>`;
+
 const initMap = () => {
   const map = new google.maps.Map($map, {
     zoom: 4,
     center: latlng(50, 10),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+  let theOnlyWindow = null;
 
   function addMarker(item) {
     const marker = new google.maps.Marker({
@@ -16,6 +25,20 @@ const initMap = () => {
       map: map,
       title: item.title
     });
+
+    if (item.venue.latitude && item.venue.longitude) {
+      const infoWindow = new google.maps.InfoWindow({
+        content: markerTpl(item)
+      });
+
+      marker.addListener('mouseover', function() {
+        if (theOnlyWindow) {
+          theOnlyWindow.close();
+        }
+        infoWindow.open(map, marker);
+        theOnlyWindow = infoWindow;
+      });
+    }
   };
 
   const events = require('./data.json');
